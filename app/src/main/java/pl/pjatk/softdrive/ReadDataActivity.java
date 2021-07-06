87e00061c35df0dd4eabc2d72ff9b62d9a39ca5b
dominiqxiqx <domste.domste@gmail.com>
2021-07-06 19:46:21 +0200

    refactor

--- app/src/main/java/pl/pjatk/softdrive/ReadDataActivity.java
+++ app/src/main/java/pl/pjatk/softdrive/ReadDataActivity.java
@@ -1,18 +1,13 @@
 package pl.pjatk.softdrive;
 
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import pl.pjatk.softdrive.rest.services.RestDistanceService;
 
-//https://stackoverflow.com/questions/25332078/how-to-the-send-data-to-the-activity-from-background-service/25332288
 public class ReadDataActivity extends AppCompatActivity {
 
 
@@ -36,7 +31,6 @@ public class ReadDataActivity extends AppCompatActivity {
         };
 
 
-
         IntentFilter intentFilter = new IntentFilter("SendDistanceDataAction");
         registerReceiver(distanceReceiver, intentFilter);
 
@@ -44,67 +38,13 @@ public class ReadDataActivity extends AppCompatActivity {
 
     }
 
-//    public class ServiceToActivity extends BroadcastReceiver
-//    {
-//        @Override
-//        public void onReceive(Context context, Intent intent)
-//        {
-//            Bundle notificationData = intent.getExtras();
-//            assert notificationData != null;
-//            String newData  = notificationData.getString("DistanceData");
-//
-//            System.out.println("test from activity");
-//            // newData is from the service
-//
-//        }
-//    }
+
 
     @Override
     protected void onDestroy() {
 
         super.onDestroy();
         //unregisterReceiver(serviceReceiver);
-
     }
 
-
-//    public class AlarmReceiver extends  {
-//
-//        @Override
-//        public void onReceive(Context context, Intent intent)
-//        {
-//
-//            // Your code to execute when the alarm triggers
-//            // and the broadcast is received.
-//            // perform your operations here.
-//
-//            // we need to reschedule again
-//            // set the timeToFetchData
-//            // call scheduleAlarm
-//
-//            Bundle notificationData = intent.getExtras();
-//            assert notificationData != null;
-//            String newData  = notificationData.getString("DistanceData");
-//
-//            System.out.println("test from activity " + newData);
-//
-//            ////timeToFetchData = 5000;
-//            //scheduleAlarm(10000L);
-//        }
-//    }
-
-
-//    public void startAlert(View view) {
-//        //EditText text = (EditText) findViewById(R.id.time);
-//        //int i = Integer.parseInt(text.getText().toString());
-//        int i = 1;
-//        Intent intent = new Intent(this, MyBroadcastReceiver.class);
-//        PendingIntent pendingIntent = PendingIntent.getBroadcast(
-//                this.getApplicationContext(), 234324243, intent, 0);
-//        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-//        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
-//                + (i * 1000), pendingIntent);
-//        Toast.makeText(this, "Alarm set in " + i + " seconds",
-//                Toast.LENGTH_LONG).show();
-//    }
 }
--- app/src/main/java/pl/pjatk/softdrive/rest/services/RestDistanceService.java
+++ app/src/main/java/pl/pjatk/softdrive/rest/services/RestDistanceService.java
@@ -1,21 +1,12 @@
 package pl.pjatk.softdrive.rest.services;
 
-import android.app.AlarmManager;
 import android.app.IntentService;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.os.PowerManager;
-import android.util.Log;
-import android.widget.Toast;
-
 import pl.pjatk.softdrive.rest.IFromRestCallback;
 import pl.pjatk.softdrive.rest.controllers.RestDistanceCtrl;
 import pl.pjatk.softdrive.rest.domain.Distance;
 import pl.pjatk.softdrive.rest.domain.Scan2d;
+
 
 public class RestDistanceService extends IntentService {
 
@@ -43,7 +34,6 @@ public class RestDistanceService extends IntentService {
                 public void getDistanceResponse(Distance value) {
                     // send broadcast distance data after call RestDistanceCtrl
                     System.out.println("distance was found : " + value.getDistance());
-
                     sendDistance(String.valueOf(value.getDistance()));
 
                 }
@@ -88,66 +78,6 @@ public class RestDistanceService extends IntentService {
         sendBroadcast(broadcastIntent);
     }
 
-    public void scheduleAlarm(Long timeToFetchData)
-    {
-        final String TAG = "schedulerAlarm:WakeLogTag";
-        // acquire wakelock
-        PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
-        assert pm != null;
-        PowerManager.WakeLock mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
-        mWakeLock.acquire( /*10 minutes*/);
-        try{
-            // The time at which the alarm will be scheduled.
-            // example every 30 secs = 30000 long as time
-            Long time = timeToFetchData;
-
-            // Create an Intent and set the class that will execute when the Alarm triggers. Here we have
-            // specified AlarmReceiver in the Intent. The onReceive() method of this class will execute when the broadcast from your alarm is received.
-            Intent intentAlarm = new Intent(getApplicationContext(), AlarmReceiver.class);
-
-            // Get the Alarm Service.
-            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-            // Set the alarm for a particular time.
-            alarmManager.set(AlarmManager.RTC_WAKEUP, time, PendingIntent.getBroadcast(getApplicationContext(), 1, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
-            Toast.makeText(getApplicationContext(), "Alarm Scheduled for 30 seconds", Toast.LENGTH_LONG).show();
-        }catch(Exception ex){}
-        finally{
-            // release wake lock
-            mWakeLock.release();
-
-        }
-    }
-
-    public class AlarmReceiver extends BroadcastReceiver {
-
-        public AlarmReceiver() {}
-
-        @Override
-        public void onReceive(Context context, Intent intent)
-        {
-
-            // Your code to execute when the alarm triggers
-            // and the broadcast is received.
-            // perform your operations here.
-
-            // we need to reschedule again
-            // set the timeToFetchData
-            // call scheduleAlarm
-
-            Bundle notificationData = intent.getExtras();
-            assert notificationData != null;
-            String newData  = notificationData.getString("DistanceData");
-
-            System.out.println("test from service time " + newData);
-
-            ////timeToFetchData = 5000;
-            //scheduleAlarm(10000L);
-        }
-    }
-
-    public AlarmReceiver alarmReceiverConstruct() {
-        return new AlarmReceiver();
-    }
-
 }
+
+
