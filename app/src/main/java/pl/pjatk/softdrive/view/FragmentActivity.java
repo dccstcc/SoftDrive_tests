d6ab68e65934ec70d509cf4f6d9542206bc44dc5
dccstcc <dccstcc@gmail.com>
2021-07-19 13:25:38 +0200

    add CannonGame templete

--- app/src/main/java/pl/pjatk/softdrive/view/FragmentActivity.java
+++ app/src/main/java/pl/pjatk/softdrive/view/FragmentActivity.java
@@ -1,5 +1,6 @@
 package pl.pjatk.softdrive.view;
 
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,4 +27,43 @@ public class FragmentActivity extends Fragment {
         trafficView = (TrafficView) view.findViewById(R.id.trafficView);
         return view;
     }
+
+    // set up volume control once Activity is created
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // allow volume buttons to set game volume
+        getActivity().setVolumeControlStream(AudioManager.STREAM_MUSIC);
+    }
+
+    // when MainActivity is paused, terminate the game
+    @Override
+    public void onPause() {
+        super.onPause();
+        trafficView.stopGame(); // terminates the game
+    }
+
+    // when MainActivity is paused, MainActivityFragment releases resources
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        trafficView.releaseResources();
+    }
 }
+
+/*********************************************************************************
+ * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and * Pearson Education, *
+ * Inc. All Rights Reserved. * * DISCLAIMER: The authors and publisher of this   *
+ * book have used their * best efforts in preparing the book. These efforts      *
+ * include the * development, research, and testing of the theories and programs *
+ * * to determine their effectiveness. The authors and publisher make * no       *
+ * warranty of any kind, expressed or implied, with regard to these * programs   *
+ * or to the documentation contained in these books. The authors * and publisher *
+ * shall not be liable in any event for incidental or * consequential damages in *
+ * connection with, or arising out of, the * furnishing, performance, or use of  *
+ * these programs.                                                               *
+ *********************************************************************************/
+
+
+
